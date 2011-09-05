9c599ea28f3bbfddde6865b2a96f4d4eed716b3d
Alexandr Krupenkin <alexandr.krupenkin@gmail.com>
2011-09-05 21:32:04 +0400

    add GPL headers & license file

--- core/board.h
+++ core/board.h
@@ -1,3 +1,23 @@
+/*
+ *      This file is part of the Netland-NG.
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ *
+ */
+
 #ifndef BOARD_H
 #define BOARD_H
 
@@ -11,23 +31,24 @@ class BoardChannel;
 class BoardMessage;
 class Connector;
 
-class Board : public QObject
+class Board
+    : public QObject
 {
-    Q_OBJECT
+  Q_OBJECT
 public:
-    explicit Board(Connector *conn);
-    inline QList<shared_ptr<BoardChannel> > channels()
-    {
-        return _channels.values();
-    }
-    inline shared_ptr<BoardChannel> getChannel(int id)
-    {
-        return _channels[id];
-    }
-    inline shared_ptr<BoardMessage> getMessage(int id)
-    {
-        return _messages[id];
-    }
+  explicit Board(Connector *conn);
+  inline QList<shared_ptr<BoardChannel> > channels()
+  {
+    return _channels.values();
+  }
+  inline shared_ptr<BoardChannel> getChannel(int id)
+  {
+    return _channels[id];
+  }
+  inline shared_ptr<BoardMessage> getMessage(int id)
+  {
+    return _messages[id];
+  }
 
 signals:
     void channelsUpdated();
